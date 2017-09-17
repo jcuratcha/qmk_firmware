0b2a55da898aac8faef95f48928f2ae9b72affec
John Curatcha <jcuratcha_@live.com>
2017-09-17 14:18:53 -0500

    Rearranged Mouse Layer to make it more convenient.

--- keyboards/planck/keymaps/jcuratcha/keymap.c
+++ keyboards/planck/keymaps/jcuratcha/keymap.c
@@ -180,19 +180,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Mouse keys
  * ,-----------------------------------------------------------------------------------.
- * |      |      | M Up |      |      |      |      | Spd0 | Spd1 | Spd2 |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | M Lf | M Dn | M Ri |      |      |      | MB 1 | MB 2 | MB 3 |      |Qwerty|
+ * |      | M Lf | M Dn | M Up | M Ri |      |      | MB 1 | MB 2 | MB 3 |      |Qwerty|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | MW L | MW D | MW U | MW R |Colemk|
+ * |      | MW L | MW D | MW U | MW R |      |      | Spd0 | Spd1 | Spd2 |      |Colemk|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |  Sym |             |   No |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_MOUSE] = {
-  {XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX},
-  {XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R,  XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, QWERTY },
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, COLEMAK},
+  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX},
+  {XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, QWERTY },
+  {XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, COLEMAK},
   {KC_LCTL, KC_LGUI, KC_LALT, FUNCTION, SYMBOL,  KC_SPC,  KC_SPC,  NUMBER,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 }
 };
