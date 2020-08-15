8ba86c3b2ea32d8f3489faab6ae3144d5ae7d1f9
John Curatcha <curatchaj@gmail.com>
2020-08-15 00:58:55 -0500

    - clean up commented diagrams to match with the actual programming - In the function, navigate and adjust layers, any transparent alphanumeric keys were converted to be no-op keys - In the function layer, when the rotary encoder/bottom left-most key is pressed, the current media is paused. Original function where the media is muted is still there. - Removed the terminal keys, since those were not really being used. - Switched the backspace and delete keys in the colemak layer. - Removed delete key codes from other layers, this is a transparent key in all of the layers so that the qwerty/colemak delete is used.

--- keyboards/preonic/keymaps/jcuratcha/keymap.c
+++ keyboards/preonic/keymaps/jcuratcha/keymap.c
@@ -41,43 +41,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |  Esc |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Nav  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | Func | Sym  |    Space    | No   | Nav  | Ctrl | Alt  | Enter|
+ * | Mute | GUI  | Alt  | Func | Sym  |    Space    | No   | Nav  | Ctrl | Alt  | Enter|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,   KC_1,    KC_2,    KC_3,     KC_4,   KC_5,   KC_6,   KC_7,   KC_8,     KC_9,    KC_0,    KC_BSPC,  \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    KC_DEL,   \
-  NAVIGATE, KC_A,    KC_S,    KC_D,     KC_F,   KC_G,   KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT,  \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,  \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,     KC_7,   KC_8,     KC_9,    KC_0,    KC_BSPC,  \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,     KC_U,   KC_I,     KC_O,    KC_P,    KC_DEL,   \
+  NAVIGATE, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,     KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT,  \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,     KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,  \
   KC_MUTE,  KC_LCTL, KC_LGUI, KC_LALT, SYMBOL, KC_SPC, NAVIGATE, NUMBER, FUNCTION, KC_RCTL, KC_RALT, KC_ENT    \
 ),
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |RShift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | Func | Sym  |    Space    | No   | Nav  | Alt  | Ctrl | Enter|
+ * | Mute | GUI  | Alt  | Func | Sym  |    Space    | No   | Nav  | Alt  | Ctrl | Enter|
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_preonic_grid( \
-  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,     KC_9,    KC_0,    KC_DEL,   \
-  KC_TAB,   KC_Q,    KC_W,    KC_F,     KC_P,   KC_G,   KC_J,   KC_L,   KC_U,     KC_Y,    KC_SCLN, KC_BSPC,  \
-  NAVIGATE, KC_A,    KC_R,    KC_S,     KC_T,   KC_D,   KC_H,   KC_N,   KC_E,     KC_I,    KC_O,    KC_QUOT,  \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,  \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,     KC_7,   KC_8,     KC_9,    KC_0,    KC_BSPC,   \
+  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,   KC_G,   KC_J,     KC_L,   KC_U,     KC_Y,    KC_SCLN, KC_DEL,  \
+  NAVIGATE, KC_A,    KC_R,    KC_S,    KC_T,   KC_D,   KC_H,     KC_N,   KC_E,     KC_I,    KC_O,    KC_QUOT,  \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_K,     KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,  \
   KC_MUTE,  KC_LCTL, KC_LGUI, KC_LALT, SYMBOL, KC_SPC, NAVIGATE, NUMBER, FUNCTION, KC_RCTL, KC_RALT, KC_ENT    \
 ),
 
@@ -123,32 +123,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______   \
 ),
 
-/* Function
+/* Function (no transparent alphas)
  * ,-----------------------------------------------------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | Caps | Num  | LED  | PrSc | Scrl |Pause |      |      |  F11 |  F12 |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      | Caps | Num  | LED  |      |      |      |      |      |  F11 |  F12 |      |
+ * |------+------+------+----- +------+-------------+------+------+------+------+------|
+ * |      | PrSc | Scrl |Pause |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |MedPly|      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNCTION] = LAYOUT_preonic_grid ( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-  _______, KC_CAPS, KC_NLCK, BACKLIT, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, KC_F11,  KC_F12,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, KC_CAPS, KC_NLCK, BACKLIT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,  _______, \
+  _______, KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, _______, \
+  KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
-/* Navigate
+/* Navigate (no transparent alphas)
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  | PgUp | Home | Up   | End  |      | PgUp | Home | Up   | End  | Del  |      |
+ * |      | PgUp | Home | Up   | End  |      | PgUp | Home | Up   | End  |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | PgDn | Left | Down | Right|      | PgDn | Left | Down | Right| Ins  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -158,32 +158,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NAVIGATE] = LAYOUT_preonic_grid ( \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  _______, \
-  _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, _______, \
+  _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
-//TODO: determine whether this layer should be removed, or if some of the contents can be removed instead.
-/* Adjust (Lower + Raise)
+/* Adjust (Sym + Num) (no transparent alphas)
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      |Reset |Debug |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|      |      |      |
+ * |      |Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
+ * |      |Voice-|Voice+|MusMod|Mus on|MusOff|MidiOn|MidOff|      | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______,  _______, _______, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, RESET,   DEBUG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, MUV_DE,  MUV_IN,  MU_MOD,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  XXXXXXX, KC_VOLD, KC_VOLU, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
@@ -261,6 +260,13 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_WH_U);
     else
       tap_code(KC_WH_D);
+  }
+  else if (IS_LAYER_ON(_FUNCTION))
+  {
+    if (clockwise)
+      tap_code(KC_MNXT);
+    else
+      tap_code(KC_MPRV);
   }
   else
   {
